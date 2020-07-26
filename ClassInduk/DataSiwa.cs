0bb6fd367654452b7abfabcc6fb133df0ae27b6a
Anjar1912 <61839743+Anjar1912@users.noreply.github.com>
2020-07-26 11:43:16 +0700

    UAS Pemrograman

---
+++ ClassInduk/DataSiwa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasUAS.KelasInduk
+{
+    public abstract class DataSiswa
+    {
+        public string Nama { get; set; }
+        public string Nis { get; set; }
+        public string Kelas { get; set; }
+        public abstract double Nilai();
+    }
+}
+
