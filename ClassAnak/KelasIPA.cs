bae182a401f4857cddbf9efb1e4b916067ef5852
Anjar1912 <61839743+Anjar1912@users.noreply.github.com>
2020-07-26 11:41:40 +0700

    UAS Pemrograman

--- ClassAnak/KelasIPA.cs
+++ ClassAnak/KelasIPA.cs
@@ -1,1 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasUAS.KelasInduk;
 
+namespace TugasUAS.KelasAnak
+{
+    public class KelasIPA : DataSiswa
+    {
+        public double NMatematika { get; internal set; }
+        public double NBiologi { get; internal set; }
+        public double NFisika { get; internal set; }
+        public double NKimia { get; internal set; }
+        public override double Nilai()
+        {
+            return (NMatematika + NBiologi + NFisika + NKimia) / 4;
+        }
+    }
+}
---
+++ ClassAnak/KelasIPS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasUAS.KelasInduk;
+
+namespace TugasUAS.KelasAnak
+{
+    public class KelasIPS : DataSiswa
+    {
+        public double NMatematika { get; set; }
+        public double NEkonomi { get; set; }
+        public double NSejarah { get; set; }
+        public double NGeografi { get; set; }
+
+        public override double Nilai()
+        {
+            return (NMatematika + NEkonomi + NSejarah + NGeografi) / 4;
+        }
+    }
+}
